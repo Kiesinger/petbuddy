0764d4782eeacad8e1c453ae0328c6b49f7db50f
Kiesinger <petbuddy.regional@gmail.com>
2025-05-14 17:29:22 +0200

    window script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Supabase-Initialisierung
 const SUPABASE_URL = 'https://rkghjywutskfwwtuzpnt.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJrZ2hqeXd1dHNrZnd3dHV6cG50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMjQwMDAsImV4cCI6MjA2MjgwMDAwMH0.QpRLi5TzPsvpFCzOilHqsaXw9Y4dv1NWflmO6Z0EkI0';
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // Login und Registrierung
 document.getElementById('loginBtn').addEventListener('click', async () => {
